e8494f48670ef56385562d37672241a5a087ce08
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 14:59:07 +0800

    commit 2

---
+++ src/M2Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class M2Q1 {
+
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int sum =0;
+        for (int i=0;i<=n;i+=3){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,14 @@
-//import java.util.Scanner;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        Scanner scn = new Scanner(System.in);
-//        int sum = 0;
-//        for (int i=1;i<=100;i++) {
-//            sum = sum + i;
-//        }
-//        System.out.println(sum);
-//    }
-//}
-
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String args[]){
+public class Main {
+
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int sum =0;
-        for (int i=0;i<=n;i+=3){
-            sum = sum+i;
-    }
+        int sum = 0;
+        for (int i=1;i<=100;i++) {
+            sum = sum + i;
+        }
         System.out.println(sum);
     }
 }
+
