e3415fa6c5f7f823ea2511c00e25366367893790
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 14:53:51 +0800

    secand commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner scn = new Scanner(System.in);
+//        int sum = 0;
+//        for (int i=1;i<=100;i++) {
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
 import java.util.Scanner;
 
-public class Main {
-
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String args[]){
         Scanner scn = new Scanner(System.in);
-        int sum = 0;
-        for (int i=1;i<=100;i++) {
-            sum = sum + i;
-        }
+        int n = scn.nextInt();
+        int sum =0;
+        for (int i=0;i<=n;i+=3){
+            sum = sum+i;
+    }
         System.out.println(sum);
     }
 }
